29bb16c80ea99c5a3ba1af0ecc447229d78858e9
pradeepkarn <mail2pkarn@gmail.com>
2024-01-02 18:14:43 +0530

    remove

--- controllers/api/Product_api.api.php
+++ controllers/api/Product_api.api.php
@@ -805,7 +805,7 @@ class Product_api
             //     OR JSON_UNQUOTE(JSON_EXTRACT(chat_history.jsn, '$.receiver_id')) = '$myid')
             //  ORDER BY chat_history.created_at;";
 
-            $sql = "SELECT JSON_UNQUOTE(JSON_EXTRACT(ch.jsn, '$.receiver_id')) as receiver_id,
+            $sql = "SELECT 
     MAX(u.first_name) as first_name,
     MAX(u.last_name) as last_name,
     MAX(ch.message) as last_message,
